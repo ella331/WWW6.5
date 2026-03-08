6643002e808f76163ac574e4e1cca10dcf87bd56
Ella Wang <160136619+ella331@users.noreply.github.com>
2026-03-08 11:25:30 +0800

    Update Day4_AuctionHouse.sol

--- ellawang/Day4_AuctionHouse.sol
+++ ellawang/Day4_AuctionHouse.sol
@@ -2,52 +2,52 @@
 pragma solidity ^0.8.0;
 
 contract AuctionHouse {
-  address public owner;
-  string public item;
-  uint public auctionEndTime;
-  address private highestBidder;
-  uint private highestBid;
-  bool public ended;
+    address public owner;
+    string public item;
+    uint public auctionEndTime;
+    address private highestBidder;
+    uint private highestBid;
+    bool public ended;
 
-  mapping(address => uint) public bids;
-  address[] public bidders;
+    mapping(address => uint) public bids;
+    address[] public bidders;
 
-  constractor(string momory _item, uint _biddingTime) {
-    owener = msg.sender;
-    item = _item;
-    auctionEndTime = block.timestamp + _biddingTime;
-  }
-
-  fuction bid(uint amount) external {
-    require(block.timestamp < auctionEndTime, "Auction has already ended.");
-    require(amount > 0, "Bid amount must be greater than zero.");
-    require(amount > bids[msg.sender], "New bid must be higher than your current bid.");
-
-    if (bids[msg.sender] == 0 {
-      bidders.push(msg.sender);
+    constructor(string memory _item, uint _biddingTime){
+        owner = msg.sender;
+        item = _item;
+        auctionEndTime = block.timestamp + _biddingTime;
     }
 
-    bids[msg.sender] = amount;
+    function bid(uint amount) external {
+        require(block.timestamp < auctionEndTime, "Auction has already ended.");
+        require(amount > 0, "Bid mount must be greater than zero.");
+        require(amount > bids[msg.sender], "New bid must be higher than your current bid.");
 
-    if (amount > highestBid) {
-      highestBid = amount;
-      highesrBidder = msg.sender;
+        if (bids[msg.sender] == 0){
+            bidders.push(msg.sender);
+        }
+
+        bids[msg.sender] = amount;
+
+        if (amount > highestBid) {
+            highestBid = amount;
+            highestBidder = msg.sender;
+        }
     }
-  }
 
-  function endAuction() external {
-    require(block.timestamp >= auctionEndTime, "Auction hasn't ended yet.");
-    require(!end, "Auction end already called.");
+    function endAuction() external {
+        require(block.timestamp >= auctionEndTime, "Auction hasn't ended yet.");
+        require(!ended, "Auction end already called.");
 
-    ended = true;
-  }
+        ended = true;
+    }
 
-  functioj getAllBidders() external view returns (address[] memory) {
-    return bidders;
-  }
+    function getAllBidders() external view returns (address[] memory) {
+        return bidders;
+    }
 
-  function getWinner() external view retuens (address, uint) {
-    require(ended, "Auntion has not ended yet.");
-    return (highestBidder, highestBid);
-  }
+    function getWinner() external view returns (address, uint) {
+        require(ended, "Auction has not ended yet.");
+        return (highestBidder, highestBid);
+    }
 }
